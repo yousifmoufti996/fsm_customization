cc4490756689c4a6cd853f1482b2b31016729bc0
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2025-09-02 04:52:59 +0300

    Update fsm_order_assignment.py

--- models/fsm_order_assignment.py
+++ models/fsm_order_assignment.py
@@ -16,14 +16,14 @@ class FSMOrder(models.Model):
         'hr.employee',  
         string='Supervisor',
         tracking=True,
-        ondelete='cascade',
+        ondelete='set null',
         help='Manager overseeing this order'
     )
     worker_id = fields.Many2one(
         'hr.employee',  # Changed from 'res.partner'
         string='Worker',
         tracking=True,
-        ondelete='cascade',
+        ondelete='set null',
         help='Worker assigned to execute this order'
     )
 
@@ -33,7 +33,7 @@ class FSMOrder(models.Model):
         string='Auditor',
         domain="[('user_id.groups_id.name', 'in', ['Auditor User', 'Access Rights', 'Settings'])]",
         tracking=True,
-        ondelete='cascade',
+        ondelete='set null',
         default=lambda self: self._get_default_auditor(),
         help='المستخدم المسؤول عن تدقيق الطلب'
     )
